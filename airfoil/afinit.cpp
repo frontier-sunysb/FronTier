1a69dbbce248bda3b87be68de24bbe408e4fe2c4
linli <linli@ams.sunysb.edu>
2013-04-05 09:08:43 -0400

    Added curve and node extra data in output for restart.

--- airfoil/afinit.cpp
+++ airfoil/afinit.cpp
@@ -575,6 +575,33 @@ extern void printAfExtraDada(
                 fprintf(outfile,"%24.18g ",sr->impulse[i]);
 	    fprintf(outfile,"\n");
 	}
+	fprintf(outfile,"\nCurve extra data:\n");
+	for (c = intfc->curves; c && *c; ++c)
+	{
+	    C_PARAMS *c_params = (C_PARAMS*)(*c)->extra;
+	    if (c_params == NULL)
+                fprintf(outfile,"curve extra: no\n");
+	    else
+	    {
+                fprintf(outfile,"curve extra: yes\n");
+                fprintf(outfile,"point_mass = %24.18g\n",c_params->point_mass);
+                fprintf(outfile,"load_mass = %24.18g\n",c_params->load_mass);
+                fprintf(outfile,"load_type = %d\n",c_params->load_type);
+                fprintf(outfile,"dir = %d\n",c_params->dir);
+	    }
+	}
+	fprintf(outfile,"\nNode extra data:\n");
+	for (n = intfc->nodes; n && *n; ++n)
+	{
+	    AF_NODE_EXTRA *n_params = (AF_NODE_EXTRA*)(*n)->extra;
+	    if (n_params == NULL)
+                fprintf(outfile,"node extra: no\n");
+	    else
+	    {
+                fprintf(outfile,"node extra: yes\n");
+                fprintf(outfile,"af_node_type = %d\n",n_params->af_node_type);
+	    }
+	}
 }	/* end printAfExtraDada */
 
 extern void readAfExtraDada(
@@ -592,6 +619,7 @@ extern void readAfExtraDada(
 	CURVE **c;
 	NODE **n;
 	BOND *b;
+	char string[100];
 
         sprintf(filename,"%s-afdata",restart_name);
         infile = fopen(filename,"r");
@@ -653,6 +681,35 @@ extern void readAfExtraDada(
                 fscanf(infile,"%lf ",&sl->impulse[i]);
             for (i = 0; i < dim; ++i)
                 fscanf(infile,"%lf ",&sr->impulse[i]);
+	}
+	next_output_line_containing_string(infile,"Curve extra data:");
+	for (c = intfc->curves; c && *c; ++c)
+	{
+	    C_PARAMS *c_params;
+	    next_output_line_containing_string(infile,"curve extra:");
+            fscanf(infile,"%s",string);
+	    if (string[0] == 'n') continue;
+	    FT_ScalarMemoryAlloc((POINTER*)&c_params,sizeof(C_PARAMS));
+	    next_output_line_containing_string(infile,"point_mass = ");
+            fscanf(infile,"%lf",&c_params->point_mass);
+	    next_output_line_containing_string(infile,"load_mass = ");
+            fscanf(infile,"%lf",&c_params->load_mass);
+	    next_output_line_containing_string(infile,"load_type = ");
+            fscanf(infile,"%d",&c_params->load_type);
+	    next_output_line_containing_string(infile,"dir = ");
+            fscanf(infile,"%d",&c_params->dir);
+	    (*c)->extra = (POINTER)c_params;
+	}
+	next_output_line_containing_string(infile,"Node extra data:");
+	for (n = intfc->nodes; n && *n; ++n)
+	{
+	    AF_NODE_EXTRA *n_params;
+	    next_output_line_containing_string(infile,"node extra:");
+            fscanf(infile,"%s",string);
+	    if (string[0] == 'n') continue;
+	    FT_ScalarMemoryAlloc((POINTER*)&n_params,sizeof(AF_NODE_EXTRA));
+	    next_output_line_containing_string(infile,"af_node_type =");
+            fscanf(infile,"%d",&n_params->af_node_type);
 	}
 }	/* end readAfExtraDada */
 
