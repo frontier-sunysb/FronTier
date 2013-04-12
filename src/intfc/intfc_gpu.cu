6941d4e9ac0d7210a8f80bac066a3c6a501ae751
linli <linli@ams.sunysb.edu>
2013-04-12 09:42:24 -0400

    Added gpu dummy to intfc and front.

---
+++ src/intfc/intfc_gpu.cu
@@ -0,0 +1,26 @@
+#include <cuda.h>
+#include <stdio.h>
+#include "intfc_gpu.cuh"
+
+__global__ void cudakernel(float *buf, const long n, const int m)
+{
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    buf[i] = 1.0f * i / n;
+    for(int j = 0; j < m; j++)
+        buf[i] = buf[i] * buf[i] - 0.25f;
+}
+
+extern void call_gpu_dummy()
+{
+        const long n = 1024 * 1024;
+        const int m = 10000;
+        float data[n];
+        float *d_data;
+
+        cudaMalloc(&d_data, n * sizeof(float));
+        cudakernel<<<n/256, 256>>>(d_data, n, m);
+        cudaMemcpy(data, d_data, n * sizeof(float), cudaMemcpyDeviceToHost);
+        cudaFree(d_data);
+
+}
+
