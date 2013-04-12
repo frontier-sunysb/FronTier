0bce4ef4fa81d6c22154a4b03f326166a49b409a
linli <linli@ams.sunysb.edu>
2013-04-12 09:17:32 -0400

    Added dim as argument for gpu_spring_solver().

--- airfoil/airfoil_gpu.cuh
+++ airfoil/airfoil_gpu.cuh
@@ -2,7 +2,8 @@
 #define _AIRFOIL_GPU_
 #include "airfoil_sv.h"
 
-extern void gpu_spring_solver(SPRING_VERTEX*,double**,double**,int,int,double);
+extern void gpu_spring_solver(SPRING_VERTEX*,double**,double**,int,int,int,
+				double);
 
 #endif
 
